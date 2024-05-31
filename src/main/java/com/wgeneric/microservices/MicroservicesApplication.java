1d8da0bba228dff095c264a283d1c8daa6639d31
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2024-05-31 13:43:59 -0500

    commit any

--- src/main/java/com/wgeneric/microservices/MicroservicesApplication.java
+++ src/main/java/com/wgeneric/microservices/MicroservicesApplication.java
@@ -15,7 +15,7 @@ public class MicroservicesApplication {
 	public GroupedOpenApi myApi() {
 		return GroupedOpenApi.builder()
 			.group("Multi Servicio")
-			.pathsToMatch("/api/**","/api/entidad/**" )
+			.pathsToMatch("/api/v1/*","/api/entidad/**" )
 			.build();
 	}
 }
